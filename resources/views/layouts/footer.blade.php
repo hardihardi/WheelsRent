187ec46ab3924b7456d8f49f69742b9ad78f60dc
hardihardi <hrdianto12@gmail.com>
2025-12-11 17:07:49 +0700

    WheelsRent

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,74 @@
+<footer class="text-light pb-5">
+    <div class="container">
+        <div class="row g-custom-x">
+            <div class="col-lg-3">
+                <div class="widget">
+                    <h5>About WheelsRent</h5>
+                    <p>
+                        Di mana kualitas bertemu dengan keterjangkauan. Kami memahami pentingnya perjalanan yang lancar
+                        dan menyenangkan tanpa beban biaya yang berlebihan. Itulah sebabnya kami telah dengan cermat
+                        membuat penawaran kami untuk menyediakan kendaraan terbaik dengan biaya minimum.
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="widget">
+                    <h5>Contact Info</h5>
+                    <address class="s1">
+                        <span><i class="id-color fa fa-map-marker fa-lg"></i>Jawa Barat, Indonesia</span>
+                        <span><i class="id-color fa fa-phone fa-lg"></i>+62 2123204018</span>
+                        <span><i class="id-color fa fa-envelope fa-lg"></i><a href="mailto:hardihardi@gmail.com">hardihardi@gmail.com</a></span>
+                        <span><i class="id-color fa fa-file-pdf fa-lg"></i><a href="#">Download Brochure</a></span>
+                    </address>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <h5>Quick Links</h5>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="widget">
+                            <ul>
+                                <li><a href="#">About</a></li>
+                                <li><a href="{{ route('mobil') }}">Mobil</a></li>
+                                <li><a href="{{ route('profile') }}">Profile</a></li>
+                                <li><a href="{{ route('contact') }}">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="widget">
+                    <h5>Social Network</h5>
+                    <div class="social-icons">
+                        <a href="https://web.facebook.com/hardihardi"><i class="fab fa-facebook"></i></a>
+                        <a href="https://twitter.com/hardihardi"><i class="fab fa-twitter"></i></a>
+                        <a href="https://www.linkedin.com/in/hardihardi/"><i class="fab fa-linkedin"></i></a>
+                        <a href="https://www.instagram.com/hardihardi/"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="subfooter">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="de-flex">
+                        <div class="de-flex-col">
+                            <a href="https://www.hardianto.site/" target="_blank">Copyright 2024 - WheelsRent by Hardianto</a>
+                        </div>
+                        <ul class="menu-simple">
+                            <li><a href="https://www.hardianto.site/">Terms &amp; Conditions</a></li>
+                            <li><a href="https://www.hardianto.site/">Privacy Policy</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
